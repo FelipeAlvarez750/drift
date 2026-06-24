e38f29c17f20baf6ec093d1aea17f3d6aba3c2e8
FelipeAlvarez750 <felipealvarez3k@gmail.com>
2026-06-24 16:34:46 -0500

    fix: include generated prisma client in repo

--- server/src/lib/prisma.ts
+++ server/src/lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '../generated/prisma';
 import { PrismaMssql } from '@prisma/adapter-mssql';
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
